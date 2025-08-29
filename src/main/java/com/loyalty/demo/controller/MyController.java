b94ae54c1367b957c5ae20ef7ba482f650a8e042
sathvik123 <sri1678622@gmail.com>
2025-08-29 16:48:47 +0530

    true

---
+++ src/main/java/com/loyalty/demo/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.loyalty.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class MyController 
+{
+	@GetMapping("/req1")
+	public String getMsg() 
+	{
+		return "welcome Sathvik";
+	}
+}
